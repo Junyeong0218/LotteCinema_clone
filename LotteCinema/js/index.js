c1ae36ed9510e30f0bed0cf7109762b114a4f509
chojoongho <gyudong1996@gmail.com>
2022-04-07 22:02:03 +0900

    2022. 04. 07
    박스모델링

--- LotteCinema/js/index.js
+++ LotteCinema/js/index.js
@@ -1,1 +1,11 @@
 const index = document.createElement("div");
+const slide_img = document.querySelector(".slide-img");
+
+window.onresize = alignImageCenter;
+
+
+function alignImageCenter(event) {
+    console.log(event);
+    const margin = (1920 - window.innerWidth) / 2;
+    slide_img.style.marginLeft = `-${margin}px`;
+}
