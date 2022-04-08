0dd5d578f2413e31cfe57d9f9d7464ed068d7973
songsaemi <songsaemiye@naver.com>
2022-04-08 16:43:08 +0900

    2022.04.08 금
    1. modal 창 작성 완료 ( click 이벤트로 modal 창 control 구현 완료 )

---
+++ LotteCinema/js/Member/login.js
@@ -0,0 +1,24 @@
+const site_list = document.querySelectorAll(".site-linker button");
+for (let i = 0; i < site_list.length; i++) {
+    site_list[i].onclick = () => {
+        site_list[i].classList.add("active");
+    }
+    site_list[i].onblur = () => {
+        site_list[i].classList.remove("active");
+    }
+}
+
+const inform_trigger = document.querySelector(".inform-trigger >button");
+const announcement_bg = document.querySelector(".announcement-bg");
+inform_trigger.onclick = () => {
+    announcement_bg.classList.add("active");
+}
+
+const announcement_closer = document.querySelector(".announcement-closer>button");
+announcement_closer.onclick = () => {
+
+    announcement_bg.classList.remove("active");
+}
+
+
+
