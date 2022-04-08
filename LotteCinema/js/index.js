af019410e8f882ace72697c44f03f8d6c895dd7b
DESKTOP-NUOPUIF\ITPS <uni0415>
2022-04-08 17:49:51 +0900

    2022. 04. 08
    banner-list 작성

--- LotteCinema/js/index.js
+++ LotteCinema/js/index.js
@@ -1,11 +1,11 @@
 const index = document.createElement("div");
 const slide_img = document.querySelector(".slide-img");
 
+window.onload = alignImageCenter;
 window.onresize = alignImageCenter;
 
-
-function alignImageCenter(event) {
-    console.log(event);
+function alignImageCenter() {
     const margin = (1920 - window.innerWidth) / 2;
+    if(margin < 440)
     slide_img.style.marginLeft = `-${margin}px`;
 }
