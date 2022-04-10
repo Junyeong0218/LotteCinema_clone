457dab4dc45a7d5122a07287b3cff541d799e958
Junyeong0218 <hippo2003@naver.com>
2022-04-10 14:02:12 +0900

    2022. 04. 10
        * event / pastEvent 작성 완료 ( html, css, js )
        * select box 및 load-more 버튼 이벤트의 경우 db 생성 및 로직 처리와 함께 작성할 예정

---
+++ LotteCinema/js/Event/past_event_tab_control.js
@@ -0,0 +1,32 @@
+const tab_buttons = document.querySelectorAll(".event-tab > li > button");
+let content_wrappers = document.querySelectorAll(".past-content-wrapper");
+let currentIndex = -1;
+console.log(tab_buttons);
+console.log(content_wrappers);
+
+for (let i = 0; i < tab_buttons.length; i++) {
+        tab_buttons[i].onclick = (event) => {
+                activeTab(event);
+                changeLayout();
+        };
+}
+
+function activeTab(event) {
+        for (let i = 0; i < tab_buttons.length; i++) {
+                if (tab_buttons[i] == event.target) {
+                        tab_buttons[i].classList.add("active");
+                        currentIndex = i;
+                }
+                else
+                        tab_buttons[i].classList.remove("active");
+        }
+}
+
+function changeLayout() {
+        for (let i = 0; i < content_wrappers.length; i++) {
+                if (i == currentIndex)
+                        content_wrappers[i].classList.remove("hidden");
+                else
+                        content_wrappers[i].classList.add("hidden");
+        }
+}
