c13118b4f171f11da28446d906740bd1acd195a1
DESKTOP-NUOPUIF\ITPS <uni0415>
2022-04-10 18:02:11 +0900

    2022. 04. 10
    슬라이드 배너 끝까지 가면 처음으로 돌아가게 이벤트 주기
    메인 네비 hover 하면 서브 메뉴 나타나게 하기

--- LotteCinema/js/index.js
+++ LotteCinema/js/index.js
@@ -3,9 +3,13 @@ const slide_img = document.querySelector(".slide-img");
 const visual_ad = document.querySelector(".visual-ad");
 const left_btn = document.querySelector(".left-btn");
 const right_btn = document.querySelector(".right-btn");
+const slide_btn = document.querySelector(".slide-btn");
+const main_nav_detail = document.querySelectorAll(".main-nav-detail");
 let prevPosition = 0;
 let imageCount = 0;
 let currentIndex = 0;
+let currentPosition = 0;
+
 
 window.onload = () => {
     alignImageCenter();
@@ -13,21 +17,40 @@ window.onload = () => {
 }
 window.onresize = alignImageCenter;
 
+
+
 right_btn.onclick = () => {
     if (currentIndex < imageCount - 1) {
-        const currentPosition = prevPosition - 1920;
-        prevPosition = currentPosition;
+        currentPosition = (currentIndex + 1) * (-1920);
         slide_img.style = `transform: translate3d(${currentPosition}px,0,0); transition: all 0.25s ease 0s;"`;
         currentIndex++;
+    } else {
+        currentPosition = 0;
+        currentIndex = 0;
+        slide_img.style = `transform: translate3d(${currentPosition}px,0,0); transition: all 0.25s ease 0s;"`;
+    }
+}
+
+left_btn.onclick = () => {
+    if (currentIndex < imageCount && currentIndex > 0) {
+        currentPosition += 1920;
+        slide_img.style = `transform: translate3d(${currentPosition}px,0,0); transition: all 0.25s ease 0s;"`;
+        currentIndex--;
+    }else {
+        currentPosition = -7680;
+        currentIndex = imageCount - 1;
+        slide_img.style = `transform: translate3d(${currentPosition}px,0,0); transition: all 0.25s ease 0s;"`;
     }
 }
 
 
-
 function alignImageCenter() {
     const margin = (1920 - window.innerWidth) / 2;
-    if(margin < 440)
-    visual_ad.style.marginLeft = `-${margin}px`;
+    if (margin < 440) {
+        visual_ad.style.marginLeft = `-${margin}px`;
+    }
 }
+
+
 
 
