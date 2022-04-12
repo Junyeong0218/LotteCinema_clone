66501294376437a5a3980c85455ec6129eb6f454
uni0415 <uni0415@naver.com>
2022-04-12 21:38:51 +0900

    2022. 04. 12 상단 네비 스크롤 시 gnb 밀려내려오는부분 수정 상단 광고 배너 새로고침시 랜덤. 중단 광고 배너 새로고침시 랜덤.

--- LotteCinema/js/index.js
+++ LotteCinema/js/index.js
@@ -4,33 +4,44 @@ const visual_ad = document.querySelector(".visual-ad");
 const left_btn = document.querySelector(".left-btn");
 const right_btn = document.querySelector(".right-btn");
 const top_banner = document.querySelector(".top-banner");
+const banner_img_wrap = document.querySelector(".banner-img-wrap");
 const banner_close_btn = document.querySelector(".banner-close");
-const main_nav_detail = document.querySelectorAll(".main-nav-detail");
 const header = document.querySelector("#header-section");
+const gnb = document.querySelector(".gnb");
+const mid_banner = document.querySelector(".banner-01 img");
+
+let height = 80;
+
 let prevPosition = 0;
 let imageCount = 0;
 let currentIndex = 0;
 let currentPosition = 0;
 
-
 window.onload = () => {
     alignImageCenter();
     imageCount = slide_img.children.length;
-
-    let banner_img = document.createElement('div');
-    banner_img.innerHTML = `<a href=""><img src="/images/top_banner_01.jpg" alt=""></a>`;
-    top_banner.appendChild(banner_img);
+    randomImg();
+    randomMidBannerImg();
 }
+
 window.onresize = alignImageCenter;
 
+window.onscroll = () => {
+    if (window.scrollY > 71+height) {
+        header.classList.add("fixed");
+        if(header.className="fixed"){
+            gnb.style.top=(-75-height)+"px";
+        }
+    } else {
+        header.classList.remove("fixed");
+        gnb.style.top="0px";
+    }     
+};
 
-let height = 80;
 banner_close_btn.onclick = () => {
     top_banner.style.display = "none";
     height = 0;
 }
-
-
 
 right_btn.onclick = () => {
     if (currentIndex < imageCount - 1) {
@@ -64,16 +75,25 @@ function alignImageCenter() {
     }
 }
 
-const gnb = document.querySelector(".gnb");
-window.addEventListener( 'scroll', () => {
-        if (window.scrollY > 71+height) {
-            header.classList.add("fixed");
-            
-        } else {
-            header.classList.remove("fixed");
-        }
+function randomImg() {
+    const img_array = ['top_banner_01.jpg','top_banner_02.jpg','top_banner_03.jpg','top_banner_04.jpg'];
+    const index = Math.floor(Math.random()*img_array.length);
+    const random_img = img_array[index];
+    const background_color_array = [
+        "242, 191, 181", "191, 226, 245", "0, 0, 0", "0, 0, 0"];
+    let banner_img = document.createElement('div');
+    banner_img.innerHTML = 
+    `
+    <a href="">
+        <img src="/images/${random_img}" class="top-banner-img" alt="">
+    </a>`;
+        
+       top_banner.style.backgroundColor= `rgb(${background_color_array[index]})`;
     
-})
+    banner_img_wrap.appendChild(banner_img);
+}
 
-
-
+function randomMidBannerImg() {
+    const index = Math.floor(Math.random()*5);
+    mid_banner.src=`/images/under_main_banner_0${index+1}.jpg`;
+}
